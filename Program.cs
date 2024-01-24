4425ec965c68bb85c660cf420a1bdd20b99b7aac
Walyson Vieira <walysonvieira29@gmail.com>
2024-01-24 01:25:12 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeiro valor: ");
+        }
+    }
+}
